1b7d45c3589f6c68f17e87d61934be9d6e5e5273
Joseph Chan <josephchan36@gmail.com>
2023-03-27 21:18:51 -0700

    added build

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/index.css'
-import {Link} from "react-router-dom"
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 function NavBar() {
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/">Homes</Link></li>
         <li><Link to="/create">Create</Link></li>
         <li><Link to="/profile">Profile</Link></li>
       </ul>
